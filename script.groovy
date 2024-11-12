b075020fd6a5e936b90bbb01430f039d3fe85fdd
Prabhat <prabhatkumar.roy@outlook.com>
2024-11-13 05:19:03 +0530

    first commit

--- script.groovy
+++ script.groovy
@@ -82,7 +82,7 @@ def ecr() {
 def update () {
          sh '''
                 cat petshop-deployment.yaml
-                sed -i 's|image: .*|image: "${BUILD_NUMBER}"|' petshop-deployment.yaml
+                 sh 'sed -i "s/TAG/${BUILD_NUMBER}/" petshop-deployment.yaml'
                 cat petshop-deployment.yaml
          '''
 }
